8395fcf2c6fb6abce49586dcabfc397ca47e5e06
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-07-15 21:52:15 +0600

    PitchBlack-ify G0130WW

--- omni_G0130WW.mk
+++ omni_G0130WW.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/runtime_libart.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := BLU
